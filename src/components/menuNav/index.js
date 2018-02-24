abb714542c36e4c8eb656b9f79f30c9511225f0e
Masooma Malik <mmalik0619@gmail.com>
2018-02-24 14:32:15 +0000

    menuNav stuff

---
+++ src/components/menuNav/index.js
@@ -0,0 +1,42 @@
+// import preact
+import { h, render, Component } from 'preact';
+import style_iphone from '../button/style_iphone';
+import style from './style.css';
+
+export default class sidenav extends Component {
+
+	render() {
+		return (
+			<div>
+				<div id="MenuNav" class="menuNav">
+					<a href="#" class="closeButton" onclick="closeMenu()">&times;
+					</a>
+					<div class="links">
+						<a href="#" >Unit Preferences</a>
+						<a href="#" >Settings</a>
+					</div>
+				</div>
+
+				<div>
+					<span onclick="openMenu()">
+						<div class="openButton"></div>
+						<div class="openButton"></div>
+						<div class="openBotton"></div>
+					</span>
+				</div>
+			</div>
+		);
+	}
+
+// this function opens the menu
+	openMenu() {
+		document.getElementById("MenuNav").style.width = "225px";
+	}
+
+// this function closes the menu
+	closeMenu() {
+		document.getElementById("MenuNav").style.width = "0";
+	}
+
+
+}
