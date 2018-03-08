c3fa237e207e3950ea890a390e411f7c114db9c4
Adnan Salehin <adnansalehin03@gmail.com>
2018-03-08 13:02:45 +0000

    changed API Key again and displayed moon%, pressure, etc.

--- src/components/iphone/homepage.js
+++ src/components/iphone/homepage.js
@@ -1,3 +1,14 @@
+/**
+ * new api key 8th march:
+ *
+ * Access ID
+ KYsdhz8O8WbuNwX2whwnq
+
+ Secret Key
+ INhy5b5Pwcd7fbonr5eTOp8qzpiIiHSxT7V0BC6B
+
+ */
+
 // import preact
 import { h, render, Component } from 'preact';
 
@@ -23,7 +34,7 @@ export default class home extends Component {
 		console.log("printed in home page city" + this.props.city);
 		// button display state
 		this.setState(
-		{ 
+		{
 			locate: this.props.city,
 			lat: this.props.lat,
 			lon: this.props.lon,
@@ -31,7 +42,7 @@ export default class home extends Component {
 		});
 		//this.onChange = (address) => this.setState({ address });
 		this.fetchWeatherData();
-	
+
 	}
 
 	fetchWeatherData = () => {
@@ -47,7 +58,7 @@ export default class home extends Component {
 		else
 		{
 			$.ajax({
-				url: "http://api.aerisapi.com/observations/closest?p="+this.state.lat+","+this.state.lon+"&client_id=4waAyyUF8UdcBAqo0q01g&client_secret=iLQq1uFp5QwPqKQKivFDbTUSiOpqBUzsbeDTCwTk",
+				url: "http://api.aerisapi.com/observations/closest?p="+this.state.lat+","+this.state.lon+"&client_id=KYsdhz8O8WbuNwX2whwnq&client_secret=INhy5b5Pwcd7fbonr5eTOp8qzpiIiHSxT7V0BC6B",
 				dataType: "jsonp",
 				success: this.parseResponse,
 				error: function(req, err){ console.log('API call failed' + err); }
@@ -55,7 +66,7 @@ export default class home extends Component {
 			});
 		}
 	}
-	
+
 
 	// the main render method for the iphone component
 	render() {
@@ -66,10 +77,10 @@ export default class home extends Component {
 			<div id={ style.container }>
 				<div id = { style.header }>
 					<div id = { style.menu  }>
-						
+
 						<Link href = {'/'} class={style.buttonHome}> </Link>
 						<Link href = {'/locations'} class={style.buttonLocation}>  </Link>
-					
+
 					</div>
 					<div class={style.relative}>
 						<p>{ this.state.date }</p>
@@ -80,17 +91,17 @@ export default class home extends Component {
 					</div>
 				</div>
 				<div id = {style.section}>
-					<div class = {style.scrollmenu}>	
-						<img src = {this.state.icon0}></img> <img src = {this.state.icon1}></img> <img src = {this.state.icon2}></img> <img src = {this.state.icon3}></img> <img src = {this.state.icon4}></img> <img src = {this.state.icon5}></img> <img src = {this.state.icon6}></img> <img src = {this.state.icon7}></img> <img src = {this.state.icon8}></img> <img src = {this.state.icon9}></img> <img src = {this.state.icon10}></img> <img src = {this.state.icon11}></img> 
+					<div class = {style.scrollmenu}>
+						<img src = {this.state.icon0}></img> <img src = {this.state.icon1}></img> <img src = {this.state.icon2}></img> <img src = {this.state.icon3}></img> <img src = {this.state.icon4}></img> <img src = {this.state.icon5}></img> <img src = {this.state.icon6}></img> <img src = {this.state.icon7}></img> <img src = {this.state.icon8}></img> <img src = {this.state.icon9}></img> <img src = {this.state.icon10}></img> <img src = {this.state.icon11}></img>
 
-						<p class = {style.inScrollText}> 	
+						<p class = {style.inScrollText}>
 							{/* must be this way otherwise it doesnt space */}
 							{this.state.temp0}° {this.state.temp1}° {this.state.temp2}° {this.state.temp3}° {this.state.temp4}° {this.state.temp5}° {this.state.temp6}° {this.state.temp7}° {this.state.temp8}°	{this.state.temp9}° {this.state.temp10}° {this.state.temp11}°
 						</p>
 					</div>
-							
+
 				</div>
-				
+
 				<div id = {style.overflowHidden}>
 					<div id = {style.overflowAuto}>
 						<div id = {style.cloudCover}>
@@ -139,9 +150,9 @@ export default class home extends Component {
 								<div class={style.phase}>
 									<p>{this.state.moonPhase}</p>
 								</div>
-								{/* <div class={style.moonPercent}>
+								<div class={style.moonPercent}>
 									<p>{this.state.moonPercent}%</p>
-								</div> */}
+								</div>
 								<div class={style.moonDistance}>
 									<p>Moon Distance: {this.state.moonDistance}km</p>
 								</div>
@@ -167,26 +178,26 @@ export default class home extends Component {
 							</div>
 							<div class={style.relativeSection}>
 								<div class={style.skyInfo}>
-									<p>{this.state.visibility}m</p>
+									<p>{this.state.visibility}mi</p>
 								</div>
 							</div>
 						</div>
 						<div id = {style.sunRise}>
 							<div class={style.pS}>
-								<p>SUNRISE AND SUNSET</p>
+								<p>SUNRISE & SUNSET</p>
 							</div>
 							<div class= {style.relativeSection}>
 								<div class= {style.aSSun}>
 									{/* <img src="rsz_sunrise.png" alt="Sunrise"/> */}
 								</div>
 								<div class={style.sunriseInfo}>
-									<p>{this.state.sunrise}</p>
+									<p>Sunrise: {this.state.sunrise}</p>
 								</div>
 								<div class={style.aSSunset}>
 									{/* <img src="rsz_sunset.png" alt="Sunset"/> */}
 								</div>
 								<div class={style.sunsetInfo}>
-									<p>{this.state.sunset}</p>
+									<p>Sunset: {this.state.sunset}</p>
 								</div>
 							</div>
 						</div>
@@ -229,28 +240,28 @@ export default class home extends Component {
 								<p>PRESSURE</p>
 							</div>
 							<div class={style.relativeSection}>
-								{/* <div class={style.pressureInfo}>
-									<p>{this.state.pressure}hPa</p>
-								</div> */}
+								<div class={style.pressureInfo}>
+									<p>{this.state.pressure} hPa</p>
+								</div>
 							</div>
 						</div>
 						<div id = {style.astroTwilight}>
 							<div class={style.pS}>
-								<p>Moon Rise and Moon Set</p>
+								<p>MOONRISE & MOONSET</p>
 							</div>
 							<div class={style.relativeSection}>
 								<div class={style.start}>
-									<p>Moonrise</p>
+									<p>Moonrise: </p>
 								</div>
-								{/* <div class={style.aTStartInfo}>
+								<div class={style.aTStartInfo}>
 									<p>{this.state.moonrise}</p>
-								</div> */}
-								{/* <div class={style.end}>
-									<p>Moonset</p>
+								</div>}
+								<div class={style.end}>
+									<p>Moonset: </p>
 								</div>
 								<div class={style.aTEndInfo}>
 									<p>{this.state.moonset}</p>
-								</div> */}
+								</div>
 							</div>
 						</div>
 					</div>
@@ -259,29 +270,26 @@ export default class home extends Component {
 		);
 	}
 
-	
+
 	iconRoot = "/assets/weatherIcons/";
-	// range from 0 to 2
-	cloudInterval=0;
 	parseAll = (parsed_json) =>
 	{
-		
-			//get location
-			console.log(parsed_json);
-			this.setState({
-				locate: parsed_json.city,
-				lat: parsed_json.lat,
-				lon: parsed_json.lon
-			});
+		//get location
+		console.log(parsed_json);
+		this.setState({
+			locate: parsed_json.city,
+			lat: parsed_json.lat,
+			lon: parsed_json.lon
+		});
 
-			//get everything else
-			$.ajax({
-				url: "http://api.aerisapi.com/observations/closest?p="+this.state.lat+","+this.state.lon+"&client_id=4waAyyUF8UdcBAqo0q01g&client_secret=iLQq1uFp5QwPqKQKivFDbTUSiOpqBUzsbeDTCwTk",
-				dataType: "jsonp",
-				success: this.parseResponse,
-				error: function(req, err){ console.log('API call failed' + err); }
-			});
-				
+		//get everything else
+		$.ajax({
+			url: "http://api.aerisapi.com/observations/closest?p="+this.state.lat+","+this.state.lon+"&client_id=KYsdhz8O8WbuNwX2whwnq&client_secret=INhy5b5Pwcd7fbonr5eTOp8qzpiIiHSxT7V0BC6B",
+			dataType: "jsonp",
+			success: this.parseResponse,
+			error: function(req, err){ console.log('API call failed' + err); }
+		});
+
 	}
 
 	parseResponse = (parsed_json) => {
@@ -307,7 +315,7 @@ export default class home extends Component {
 		var sunrise = path.sunriseISO;
 		var sunset = path.sunsetISO;
 		var currentCloud = path.sky;
-		var precipitation = path.precipMM;
+		var pressure = path.pressureMB;
 		var icon = path.icon;
 
 
@@ -317,25 +325,18 @@ export default class home extends Component {
 			time: (((((time.split("T")[1]).split("+"))[0]).split(":"))[0])+":"+(((((time.split("T")[1]).split("+"))[0]).split(":"))[1]),
 			temp: temp_c,
 			cond: weatherType,
-			//feels: feelsLike,
 			windDirection: this.windDirectionTypes[windDirection],
 			humidity: humidity,
-			//windGust: windGust,
 			visibility: visibility,
 			windSpeed: windSpeed,
-			//precProp: precProp
-			//moonPhase: moonPhase
 			currentCloud: currentCloud,
-			// precipitation: precipitation,
+			pressure: pressure,
 			icon: "/assets/weatherSD/"+icon,
 			sunrise: (((((sunrise.split("T")[1]).split("+"))[0]).split(":"))[0])+":"+(((((sunrise.split("T")[1]).split("+"))[0]).split(":"))[1]),
 			sunset: (((((sunset.split("T")[1]).split("+"))[0]).split(":"))[0])+":"+(((((sunset.split("T")[1]).split("+"))[0]).split(":"))[1])
 		});
 
-		//old one
-		//var url = "http://api.aerisapi.com/sunmoon/?p="+this.state.lat+","+this.state.lon+"&client_id=4waAyyUF8UdcBAqo0q01g&client_secret=iLQq1uFp5QwPqKQKivFDbTUSiOpqBUzsbeDTCwTk";
-
-		var urlMoon = "http://api.aerisapi.com/sunmoon/"+this.state.lat+","+this.state.lon+"?&client_id=4waAyyUF8UdcBAqo0q01g&client_secret=iLQq1uFp5QwPqKQKivFDbTUSiOpqBUzsbeDTCwTk";
+		var urlMoon = "http://api.aerisapi.com/sunmoon/"+this.state.lat+","+this.state.lon+"?&client_id=KYsdhz8O8WbuNwX2whwnq&client_secret=INhy5b5Pwcd7fbonr5eTOp8qzpiIiHSxT7V0BC6B";
 
 		$.ajax({
 			url: urlMoon,
@@ -344,23 +345,23 @@ export default class home extends Component {
 			error: function(req, err){ console.log('API call failed' + err); }
 		});
 		//Test url:
-		//http://api.aerisapi.com/forecasts/51.5074,-0.127758?filter=3hr&limit=3&client_id=4waAyyUF8UdcBAqo0q01g&client_secret=iLQq1uFp5QwPqKQKivFDbTUSiOpqBUzsbeDTCwTk
+		//http://api.aerisapi.com/forecasts/51.5074,-0.127758?filter=3hr&limit=3&client_id=KYsdhz8O8WbuNwX2whwnq&client_secret=INhy5b5Pwcd7fbonr5eTOp8qzpiIiHSxT7V0BC6B
 
-		var urlCloud = "http://api.aerisapi.com/forecasts/"+this.state.lat+","+this.state.lon+"?filter=3hr&limit=3&client_id=4waAyyUF8UdcBAqo0q01g&client_secret=iLQq1uFp5QwPqKQKivFDbTUSiOpqBUzsbeDTCwTk";
+		var urlCloud = "http://api.aerisapi.com/forecasts/"+this.state.lat+","+this.state.lon+"?filter=3hr&limit=3&client_id=KYsdhz8O8WbuNwX2whwnq&client_secret=INhy5b5Pwcd7fbonr5eTOp8qzpiIiHSxT7V0BC6B";
 		$.ajax({
 			url: urlCloud,
 			dataType: "jsonp",
 			success: this.parseResponseCloud,
 			error: function(req, err){ console.log('API call failed' + err); }
 		});
-		var urlPrecip = "http://api.aerisapi.com/forecasts/"+this.state.lat+","+this.state.lon+"?&client_id=4waAyyUF8UdcBAqo0q01g&client_secret=iLQq1uFp5QwPqKQKivFDbTUSiOpqBUzsbeDTCwTk";
+		var urlPrecip = "http://api.aerisapi.com/forecasts/"+this.state.lat+","+this.state.lon+"?&client_id=KYsdhz8O8WbuNwX2whwnq&client_secret=INhy5b5Pwcd7fbonr5eTOp8qzpiIiHSxT7V0BC6B";
 		$.ajax({
 			url: urlCloud,
 			dataType: "jsonp",
 			success: this.parseResponsePrecip,
 			error: function(req, err){ console.log('API call failed' + err); }
 		});
-		var urlTemp = "http://api.aerisapi.com/forecasts/"+this.state.lat+","+this.state.lon+"?filter=1hr&limit=12&client_id=4waAyyUF8UdcBAqo0q01g&client_secret=iLQq1uFp5QwPqKQKivFDbTUSiOpqBUzsbeDTCwTk";
+		var urlTemp = "http://api.aerisapi.com/forecasts/"+this.state.lat+","+this.state.lon+"?filter=1hr&limit=12&client_id=KYsdhz8O8WbuNwX2whwnq&client_secret=INhy5b5Pwcd7fbonr5eTOp8qzpiIiHSxT7V0BC6B";
 		$.ajax({
 			url: urlTemp,
 			dataType: "jsonp",
@@ -378,16 +379,17 @@ export default class home extends Component {
 		*/
 		var path = parsed_json.response[0].moon;
 		var moonPhase = path.phase.name;
+		var moonPercent = path.phase.illum;
 		var moonrise = path.riseISO;
 		var moonset = path.setISO;
 		var time = parsed_json.response[0].dateTimeISO;
-		//!!Warning: moonrise unavailable today.
+
 		this.setState({
 			time: (((((time.split("T")[1]).split("+"))[0]).split(":"))[0])+":"+(((((time.split("T")[1]).split("+"))[0]).split(":"))[1]),
-
+			moonPercent: moonPercent,
 			moonPhase: moonPhase,
-			//moonrise: (((((moonrise.split("T")[1]).split("+"))[0]).split(":"))[0])+":"+(((((moonrise.split("T")[1]).split("+"))[0]).split(":"))[1]),
-			//moonset: (((((moonset.split("T")[1]).split("+"))[0]).split(":"))[0])+":"+(((((moonset.split("T")[1]).split("+"))[0]).split(":"))[1])
+			moonrise: (((((moonrise.split("T")[1]).split("+"))[0]).split(":"))[0])+":"+(((((moonrise.split("T")[1]).split("+"))[0]).split(":"))[1]),
+			moonset: (((((moonset.split("T")[1]).split("+"))[0]).split(":"))[0])+":"+(((((moonset.split("T")[1]).split("+"))[0]).split(":"))[1])
 		});
 		console.log(parsed_json);
 
